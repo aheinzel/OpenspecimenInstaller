e30c8c4dee30596cc2e4f5bc408d5ce857b09ff3
aheinzel <mail@aheinzel.at>
2019-07-19 17:02:15 +0200

    update to openspecimen version 6.1RC5

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # OpenspecimenInstaller
-This is a simple installer (bash script) for openspecimen v5.2x on a fresh Ubuntu 16.04 installation. The installer takes care of setting up mySQL and tomcat for openspecimen, builds openspecimen from source and deploys it to the local tomcat instance.
+This is a simple installer (bash script) for openspecimen v6.1RC5 on a fresh Ubuntu 16804 installation. The installer takes care of setting up mySQL and tomcat for openspecimen, builds openspecimen from source and deploys it to the local tomcat instance.
 
 ## Configuration
 Check the shell variables at the beginning of the installer.
 
 ## CAVEATS
 * use only on a fresh Ubuntu installation
+* openjdk-8 is used
 * mySQL and tomcat are automatically installed on the local machine
 * mySQL is reconfigure to force the use of lower case table names (this could potentially brake existing application)
 * tomcat is reconfigured to use 2GB heap space
 * mySQL connector is deployed on tomcat as common library
-* the installer attempts to automatically download the mySQL connector from mysql.com. In case the zip archive is no longer available the MYSQL_CONNECTOR_* shell variables must be to be adapted. 
+* version of openspecimens dependency on grunt-contrib-imagemin is incremented to >= 1.0.0
+* the installer attempts to automatically download the mySQL connector from mysql.com. In case the zip archive is no longer available the MYSQL_CONNECTOR_* shell variables must be adapted. 
--- old/v5.2/openspecimen_installer.sh
+++ old/v5.2/openspecimen_installer.sh
@@ -2,7 +2,7 @@
 set -e
 
 #############################################################
-# INSTALLER FOR OPENSPECIMEN V6.1.RC5                       #
+# INSTALLER FOR OPENSPECIMEN V5.2.x                         #
 #                                                           #
 # Builds Openspecimen from source and deploys it on tomcat  #
 # CAVE: this script automatically installs and RECONFIGURES #
@@ -10,12 +10,11 @@ set -e
 #############################################################
 
 ### TOMCAT INFO ###
-TOMCAT_SERVICE_NAME="tomcat9"
-TOMCAT_HOME="/var/lib/tomcat9"
-TOMCAT_SHARED_LIB="/usr/share/tomcat9/lib"
-TOMCAT_SYSTEMD_UNIT_FILE="/etc/systemd/system/multi-user.target.wants/tomcat9.service"
-TOMCAT_USER="tomcat"
-TOMCAT_GROUP="tomcat"
+TOMCAT_SERVICE_NAME="tomcat8"
+TOMCAT_HOME="/var/lib/tomcat8"
+TOMCAT_SHARED_LIB="/usr/share/tomcat8/lib"
+TOMCAT_USER="tomcat8"
+TOMCAT_GROUP="tomcat8"
 
 ### MYSQL INFO ###
 MYSQLD_CONF="/etc/mysql/mysql.conf.d/mysqld.cnf"
@@ -26,27 +25,13 @@ DB_PASS="openspecimen"
 DB_NAME="openspecimen"
 
 ### OPENSPECIMEN VERSION INFO (used for checkout) ###
-OPENSPECIMEN_GIT_BRANCH="v6.1.RC5"
+OPENSPECIMEN_GIT_BRANCH="v5.2.x"
 
 ### OPENSPECIMEN SYSTEM INFO ###
 OPENSPECIMEN_APP_NAME="openspecimen"
 OPENSPECIMEN_HOME="/var/lib/openspecimen"
 OPENSPECIMEN_DATA="${OPENSPECIMEN_HOME}/data"
 OPENSPECIMEN_PLUGINS="${OPENSPECIMEN_HOME}/plugins"
-OPENSPECIMEN_PACKAGE_JSON_PATCH=$(cat <<EOF
---- package.json        2019-07-17 15:14:00.419021754 +0000
-+++ package.json.new    2019-07-18 04:27:36.961700752 +0000
-@@ -34,7 +34,7 @@
-     "grunt-contrib-copy": "^0.7.0",
-     "grunt-contrib-cssmin": "^0.10.0",
-     "grunt-contrib-htmlmin": "^0.3.0",
--    "grunt-contrib-imagemin": "0.9.1",
-+    "grunt-contrib-imagemin": "^1.0.0",
-     "grunt-contrib-uglify": "^0.6.0",
-     "grunt-contrib-watch": "^0.6.1",
-     "grunt-filerev": "^2.1.1",
-EOF
-)
 
 ### MYSQL CONNECTOR/J DOWNLOAD ###
 MYSQL_CONNECTOR_URL="https://cdn.mysql.com//Downloads/Connector-J/mysql-connector-java-8.0.15.zip"
@@ -66,22 +51,34 @@ then
    exit 1
 fi
 
+## set config for unattended install of mysql-server and java8
+#debconf can't be used to set empty mysql pw
+#debconf-set-selections <<< 'mysql-server mysql-server/root_password password root'
+#debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password root'
+debconf-set-selections <<< 'debconf shared/accepted-oracle-license-v1-1 select true'
+debconf-set-selections <<< 'debconf shared/accepted-oracle-license-v1-1 seen true'
+
+## install java 8
+apt install -y -q \
+   software-properties-common
+
+add-apt-repository -y ppa:webupd8team/java
+apt update
+apt-get install -y -q \
+   oracle-java8-installer
+
 ## install required packages
 DEBIAN_PRIORITY=critical apt install -y -q \
    mysql-server
 
-
 apt install -y -q \
-   openjdk-8-jre-headless \
-   openjdk-8-jdk-headless \
-   tomcat9 \
-   nodejs \
+   tomcat8 \
+   nodejs-legacy \
    npm \
    gradle \
    git \
    nano \
-   gawk \
-   libpng-dev
+   gawk
 
 npm install -g bower grunt-cli
 
@@ -94,17 +91,6 @@ then
 else
    sed -i '/\[mysqld\]/a lower_case_table_names=1' "${MYSQLD_CONF}"
 fi
-
-##use UTF8
-if grep 'character-set-server=.*' "${MYSQLD_CONF}"
-then
-   sed -i 's/character-set-server=.*/character-set-server=utf8/' "${MYSQLD_CONF}"
-else
-   sed -i '/\[mysqld\]/a character-set-server=utf8' "${MYSQLD_CONF}"
-fi
-
-#Note: could also set encoding for client
-
 systemctl start mysql
 
 ## create openspecimen database and user
@@ -148,21 +134,18 @@ EOF
 
 
 ## build openspecimen from source (steps are performed under user installuser)
-export TOMCAT_HOME OPENSPECIMEN_PACKAGE_JSON_PATCH OPENSPECIMEN_GIT_BRANCH
 cd /tmp
 useradd -m installuser
-su installuser << 'EOF'
+su installuser << EOF
 git clone https://github.com/krishagni/openspecimen.git
 cd openspecimen/
 git checkout "${OPENSPECIMEN_GIT_BRANCH}"
 sed -i "s@app_home=.*@app_home=${TOMCAT_HOME}@" build.properties
 cd www
-echo "${OPENSPECIMEN_PACKAGE_JSON_PATCH}" | patch
 npm install
 bower install
 cd ..
 gradle build
-gradle --stop
 EOF
 
 cd openspecimen
@@ -171,11 +154,6 @@ chown ${TOMCAT_USER}:${TOMCAT_GROUP} "${TOMCAT_HOME}/webapps/openspecimen.war"
 userdel -r installuser
 
 ## reconfigure tomcat to use 2GB heap space
-## ATT: assume JAVA_OPTS is present
-sed -i -r 's/^(JAVA_OPTS=.*)"/\1 -Xmx2048m"/' "/etc/default/${TOMCAT_SERVICE_NAME}"
-
-## allow tomcat to write to openspecimen home directory
-gawk -i inplace '{if($0 ~ /\[Service\]/){print $0; print perm;}else{print $0}}' \
-   perm="ReadWritePaths=${OPENSPECIMEN_HOME}" "${TOMCAT_SYSTEMD_UNIT_FILE}"
+sed -i -r 's/(JAVA_OPTS=.*)-Xmx[^ ]+(.*)/\1-Xmx2048m\2/' /etc/default/tomcat8
 
 systemctl start "${TOMCAT_SERVICE_NAME}"
